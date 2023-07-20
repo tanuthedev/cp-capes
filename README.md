56017c92ca3a647cef65c88750afdc78b89e4ead
Kiriko <70314098+mynameiskiriko@users.noreply.github.com>
2023-07-20 09:09:15 +0200

    1st commit
    yahoooooo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cloaks+ Capes Mod
+Unofficial fork of [OF-Capes by dragonostic](https://github.com/dragonostic/of-capes) made for Cloaks+.
---
+++ src/main/java/lol/tanu/cpcapes/capes.java
@@ -0,0 +1,11 @@
+package lol.tanu.cpcapes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class capes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
